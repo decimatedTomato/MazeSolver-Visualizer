6cc190a62f760b2bf2037a5b66c77b418a771767
decimatedTomato <greentomatopower@gmail.com>
2023-09-22 16:25:51 +0200

    Added remaining logic, let's hope that stuff works

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,11 @@ type Coordinate = {
     x: number
     y: number
 }
+type searched_cell = {
+    coord: Coordinate,
+    prev_cell: searched_cell | null,
+}
+
 function random_coordinate(x: number, y: number): Coordinate {
     return { x: Math.floor(Math.random() * x), y: Math.floor(Math.random() * y) }
 }
@@ -42,6 +47,9 @@ class Maze {
     }
     public get_cell_type(coordinate: Coordinate) {
         return maze.maze[coordinate.x][coordinate.y]
+    }
+    public set_cell_type(coordinate: Coordinate, cell_type: MazeCell) {
+        maze.maze[coordinate.x][coordinate.y] = cell_type
     }
     public get_neighboring_coordinates(coordinate: Coordinate) {
         const neighbors = []
@@ -68,6 +76,8 @@ class Maze {
             }
             this.maze.push(column)
         }
+        this.set_cell_type(this.start, MazeCell.EXPLORED)
+        this.set_cell_type(this.end, MazeCell.FLOOR)
     }
 }
 
@@ -77,46 +87,63 @@ const cell_width = canvas.width / maze.width
 const cell_height = canvas.height / maze.height
 const ctx = canvas.getContext("2d")
 function canvas_refresh() {
-    if (ctx == undefined) return
-    ctx.fillStyle = "black"
-    ctx.fill()
-    for (let i = 0; i < maze.width; i++) {
-        for (let j = 0; j < maze.height; j++) {
-            let color = "black"
-            if (maze.maze[i][j] == MazeCell.FLOOR) color = "white"
-            else if (maze.maze[i][j] == MazeCell.WALL) color = "black"
-            else if (maze.maze[i][j] == MazeCell.ACTIVE) color = "yellow"
-            else if (maze.maze[i][j] == MazeCell.EXPLORED) color = "gray"
-            if (maze.start?.x == i && maze.start?.y == j) color = "green"
-            if (maze.end?.x == i && maze.end?.y == j) color = "red"
+    function draw() {
+        if (ctx == undefined) return
+        ctx.fillStyle = "black"
+        ctx.fill()
+        for (let i = 0; i < maze.width; i++) {
+            for (let j = 0; j < maze.height; j++) {
+                let color = "black"
+                if (maze.maze[i][j] == MazeCell.FLOOR) color = "white"
+                else if (maze.maze[i][j] == MazeCell.WALL) color = "black"
+                else if (maze.maze[i][j] == MazeCell.ACTIVE) {
+                    color = "yellow"
+                    maze.maze[i][j] = MazeCell.EXPLORED
+                } else if (maze.maze[i][j] == MazeCell.EXPLORED) color = "gray"
+                if (maze.start?.x == i && maze.start?.y == j) color = "green"
+                if (maze.end?.x == i && maze.end?.y == j) color = "red"
 
-            ctx.fillStyle = color
-            ctx.fillRect(i * cell_width, j * cell_height, cell_width, cell_height)
+                ctx.fillStyle = color
+                ctx.fillRect(i * cell_width, j * cell_height, cell_width, cell_height)
+            }
         }
+    }
+    setTimeout(draw, 1);
+}
+
+function canvas_draw_path(path_cell: searched_cell) {
+    if (ctx == undefined) return
+    ctx.fillStyle = "purple"
+    while (path_cell.prev_cell != null) {
+        ctx.fillRect(path_cell.coord.x * cell_width, path_cell.coord.y * cell_height, cell_width, cell_height)
+        path_cell = path_cell.prev_cell
     }
 }
 
 function dfs() {
-    type searched_cell = {
-        coord: Coordinate,
-        prev_cell: Coordinate | null,
-    }
     if (maze.start == null) return
     let search_frontier: Array<searched_cell> = [ { coord: maze.start, prev_cell: null } ]
 
     let search_ended = false
+    let final_searched_cell: searched_cell | null = null
     while (!search_ended) {
         for (const position of search_frontier) {
-            for (const adjacent_position of maze.get_neighboring_coordinates(position)) {
-                if (maze.get_cell_type(adjacent_position) == MazeCell.FLOOR) {
-                    // Do something
+            for (const adjacent_position of maze.get_neighboring_coordinates(position.coord)) {
+                if (maze.end == adjacent_position) {
+                    final_searched_cell = {coord: adjacent_position, prev_cell: position}
+                } else if (maze.get_cell_type(adjacent_position) == MazeCell.FLOOR) {
+                    maze.set_cell_type(adjacent_position, MazeCell.ACTIVE)
+                    search_frontier.push({coord: adjacent_position, prev_cell: position})
                 }
             }
         }
         // Check if goal has been found
+        if (final_searched_cell != null) search_ended = true
         // Check if grid has been exhausted
+        if (search_frontier.length == 0) search_ended = true
         canvas_refresh()
     }
+    if (final_searched_cell != null) canvas_draw_path(final_searched_cell)
 }
 function bfs() {
     canvas_refresh()
